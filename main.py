1f2f2d190df0c7fe90e10936546efbd72ba024d2
Evgeniy <timofeeevzh@gmail.com>
2020-02-22 00:41:01 +0200

    initial

---
+++ main.py
@@ -0,0 +1,20 @@
+#@timofeeevzh
+
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Timofeev Evgeniy TS-81'
+
+@app.route('/first')
+def helloo():
+    a = None
+    b = 124124
+    c = 'fafew'
+    return f'{a}, {b}, {c}'
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
