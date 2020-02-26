fcda7d259b651dbc504fbe4d3a59829f5370c44a
Evgeniy <timofeeevzh@gmail.com>
2020-02-26 22:49:11 +0200

    initial

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #@timofeeevzh 
 
-from flask import Flask, escape, request
-
+from flask import Flask, escape, request, render_template
+import templates
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,7 +14,18 @@ def helloo():
     a = None
     b = 124124
     c = 'fafew'
-    return f'{a}, {b}, {c}'
+    return f'{a},{b},{c}'
+
+@app.route('/second', methods=['GET','POST'])
+def second():
+    if  request.method == 'GET':
+        return render_template('./second2.html')
+    if request.method == 'POST':
+        list1 = request.form.get('text')
+        turn=[x for x in reversed(list1)]
+        s=''
+        print(list1)
+        return f'{list1[::-1]}{s.join(list(reversed(list1)))}{s.join(turn)}'
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
